9503f9a984754b0fe8429b281548d4858ae72867
lincy267 <lincy0726@gmail.com>
2023-02-27 12:03:24 -0400

    updated projects of d17 classes - quiz using database from Open Trivia

--- d17/data.py
+++ d17/data.py
@@ -1,17 +1,57 @@
 question_data = [
-    {"text": "A slug's blood is green.", "answer": "True"},
-    {"text": "The loudest animal is the African Elephant.", "answer": "False"},
-    {"text": "Approximately one quarter of human bones are in the feet.", "answer": "True"},
-    {"text": "The total surface area of a human lungs is the size of a football pitch.", "answer": "True"},
-    {"text": "In West Virginia, USA, if you accidentally hit an animal with your car, "
-             "you are free to take it home to eat.", "answer": "True"},
-    {"text": "In London, UK, if you happen to die in the House of Parliament, "
-             "you are entitled to a state funeral.", "answer": "False"},
-    {"text": "It is illegal to pee in the Ocean in Portugal.", "answer": "True"},
-    {"text": "You can lead a cow down stairs but not up stairs.", "answer": "False"},
-    {"text": "Google was originally called 'Backrub'.", "answer": "True"},
-    {"text": "Buzz Aldrin's mother's maiden name was 'Moon'.", "answer": "True"},
-    {"text": "No piece of square dry paper can be folded in half more than 7 times.", "answer": "False"},
-    {"text": "A few ounces of chocolate can to kill a small dog.", "answer": "True"}
+    {
+        "category": "History",
+        "type": "boolean",
+        "difficulty": "easy",
+        "question": "The Spitfire originated from a racing plane.",
+        "correct_answer": "True",
+        "incorrect_answers": ["False"]
+    },
+    {
+        "category": "Entertainment: Music", "type": "boolean", "difficulty": "medium",
+        "question": "The country song  'A Boy Named Sue' was written by Shel Silverstein.",
+        "correct_answer": "True", "incorrect_answers": ["False"]
+    },
+    {
+        "category": "Art", "type": "boolean", "difficulty": "medium",
+        "question": "Pablo Picasso is one of the founding fathers of &quot;Cubism.&quot;",
+        "correct_answer": "True", "incorrect_answers": ["False"]
+    },
+    {
+        "category": "Entertainment: Video Games", "type": "boolean", "difficulty": "easy",
+        "question": "Deus Ex (2000) does not feature the World Trade Center because it was destroyed by "
+                    "terrorist attacks according to the game's plot.",
+        "correct_answer": "True", "incorrect_answers": ["False"]
+    },
+    {
+        "category": "Geography", "type": "boolean", "difficulty": "medium",
+        "question": "Norway has a larger land area than Sweden.",
+        "correct_answer": "False", "incorrect_answers": ["True"]
+    },
+    {
+        "category": "General Knowledge", "type": "boolean", "difficulty": "medium",
+        "question": "Haggis is traditionally ate on Burns Night.",
+        "correct_answer": "True", "incorrect_answers": ["False"]
+    },
+    {
+        "category": "Entertainment: Video Games", "type": "boolean", "difficulty": "easy",
+        "question": "In the &quot;S.T.A.L.K.E.R.&quot; series, the Freedom faction wishes to destroy "
+                    "the supernatural area known as 'the Zone'.",
+        "correct_answer": "False", "incorrect_answers": ["True"]
+    },
+    {
+        "category": "Entertainment: Music", "type": "boolean", "difficulty": "medium",
+        "question": "&quot;Neutral Milk Hotel&quot; is a real band.",
+        "correct_answer": "True", "incorrect_answers": ["False"]
+    },
+    {
+        "category": "Entertainment: Video Games", "type": "boolean", "difficulty": "hard",
+        "question": "In The Witcher 3, the Zoltan Chivay Gwent card can be found under the Hanged Man's Tree.",
+        "correct_answer": "True", "incorrect_answers": ["False"]
+    },
+    {
+        "category": "General Knowledge", "type": "boolean", "difficulty": "easy",
+        "question": "Jingle Bells was originally meant for Thanksgiving",
+        "correct_answer": "True", "incorrect_answers": ["False"]
+    }
 ]
-
